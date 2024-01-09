770a8ea8f2db1e36cd85c61fc2690d20ca2b3be9
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-01-09 11:17:40 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /bot-v2
 fi
-cd /EvaMaria
+cd /bot-v2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
